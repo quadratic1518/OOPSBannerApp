600c35bd7051ee9393faa10bb5e629806062ad71
quadratic1518 <praneethreddy1518c@gmail.com>
2026-02-16 00:43:14 +0530

    Add files via upload
    UC1: Implement PrintOOPS - Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/**
+ * OOPS Banner App - Use Case 1: Printing OOPS to Console
+ * 
+ * @author quadratic1518
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    
+    /**
+     * Main method - Entry point of the application
+     * Prints "OOPS" to the console
+     * 
+     * @param args Command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
