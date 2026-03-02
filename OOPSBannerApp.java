5c2d590d8ae790f2704b6e3df5203b620ade663b
quadratic1518 <praneethreddy1518c@gmail.com>
2026-03-02 13:04:13 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,28 @@
-/**
- * OOPS Banner App - Use Case 1: Printing OOPS to Console
- * 
- * @author quadratic1518
- * @version 1.0
- */
-public class OOPSBannerApp {
-    
-   
+public class UC1 {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String O = 
+              " *****  \n"
+            + "*     * \n"
+            + "*     * \n"
+            + "*     * \n"
+            + " *****  \n";
+
+        String P = 
+              " ****** \n"
+            + "*      *\n"
+            + "*      *\n"
+            + " ****** \n"
+            + "*       \n"
+            + "*       \n"
+            + "*       \n";
+        String S = 
+              " ****** \n"
+            + "*       \n"
+            + " *****  \n"
+            + "      * \n"
+            + " ****** \n";
+
+        System.out.println(O + O + P + S);
     }
 }
